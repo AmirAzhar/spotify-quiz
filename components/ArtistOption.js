1b928d5c6e29127667b0e293f08c6e348e87058c
Amir Azhar <amir97azhar@gmail.com>
2022-05-01 20:34:52 +0800

    Get spotify data

--- components/ArtistOption.js
+++ components/ArtistOption.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-function ArtistOption() {
+function ArtistOption({ artist }) {
+  artist = artist
+    ? artist
+    : { name: "", images: [{ url: "http://placehold.jp/640x640.png" }] };
   return (
     <div className="flex flex-col items-center">
-      <div className="h-72 w-72 rounded-full border-2 border-black hover:scale-110 transition duration-300 cursor-pointer "></div>
-      <div className="text-2xl p-5">Placeholder</div>
+      <img
+        src={artist.images[0].url}
+        className="h-72 w-72 rounded-full border-2 hover:scale-110 transition duration-300 cursor-pointer "
+      />
+      <div className="text-2xl p-5">{artist["name"]}</div>
     </div>
   );
 }
--- components/Main.js
+++ components/Main.js
@@ -1,12 +1,41 @@
-import React from "react";
+// Lib
+import React, { useEffect, useState } from "react";
+import { useSession } from "next-auth/react";
+import { shuffle } from "lodash";
+
+// Code
 import ArtistOption from "./ArtistOption";
+import useSpotify from "../hooks/useSpotify";
 
 function Main() {
+  const spotifyApi = useSpotify();
+  const { data: session, status } = useSession();
+  const [artistsData, setArtistsData] = useState([]);
+  const playListId = "37i9dQZEVXbNG2KDcFcKOF?si=e467e026c3474ac4";
+
+  useEffect(() => {
+    if (spotifyApi.getAccessToken()) {
+      spotifyApi
+        .getPlaylist(playListId)
+        .then((data) => {
+          const ids = _.shuffle(data.body.tracks.items)
+            .slice(0, 2)
+            .map((song) => song.track.artists[0].id);
+          return ids;
+        })
+        .then((ids) =>
+          spotifyApi.getArtists(ids).then((data) => {
+            setArtistsData(data.body.artists);
+          })
+        );
+    }
+  }, [session, spotifyApi]);
+
   return (
     <div className="absolute inset-x-0 top-32 h-[calc(100vh-30%)]">
       <div className="flex justify-around items-center h-full">
-        <ArtistOption />
-        <ArtistOption />
+        <ArtistOption artist={artistsData[0]} />
+        <ArtistOption artist={artistsData[1]} />
       </div>
     </div>
   );
